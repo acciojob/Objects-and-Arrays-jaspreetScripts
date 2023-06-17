b57bd92e39a6c47e00568dbdf339b1cc73e95a5d
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-17 22:24:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const players = ["John", "Bob", "Dhoni", "Dhoni"];
+const players = ["John", "Bob", "Alice", "Poppy"];
 
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-let team  = players;
+let team  = players[];
 let team1 =["John", "Bob", "Alice", "Poppy"];
 let cap1 ={
   name: "John Doe",
